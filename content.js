b735c09d144d8ac66940077338abb571d11a65c2
Michal Kruszewski <kruszewski2@gmail.com>
2019-10-04 08:56:57 +0200

    Changed some style values params
    Changed some style values params to better handle different screen
    resolutions and different users streams

--- content.js
+++ content.js
@@ -2,16 +2,24 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message,sender,sendresponse)
 {
     alert(message.message);
-    var chat = document.getElementsByClassName("_1c_u _5-yb _302m");
-    console.log(chat);
-    console.log(chat[0].style);
-    console.log(chat[0].style["width"]);
-    chat[0].style["width"] = "auto";
-    chat[0].style["height"] = "-webkit-fill-available";
-    var video = document.getElementsByClassName("_6445 _6j32");
-    video[0].style['display'] = "none";
+
+    var video = document.getElementsByTagName('video');
+    console.log(video);
+    console.log(video[0].parentNode);
+    console.log(video[0].parentNode.parentNode);
+    var parent = video[0].parentNode.parentNode;
+
+    //var video = document.getElementsByClassName("_1c_u _5-yb _302m");
+    parent.style["width"] = "auto";
+    parent.style["height"] = "-webkit-fill-available";
+
+
+    var chat = document.getElementsByClassName("_6445 _6j32");
+    chat[0].style['display'] = "none";
 
     var parentDiv = document.getElementsByClassName("_53j5");
     parentDiv[0].style['width'] = '-webkit-fill-available';
-    parentDiv[0].style['height'] = '-webkit-fill-available';
+    parentDiv[0].style['height'] = '95vh';
+
+    //parentDiv[0].style['height'] = '-webkit-fill-available';
 }
