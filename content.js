661d6b28799e6551a368cf11560b86e5606d5875
Michal Kruszewski <kruszewski2@gmail.com>
2019-10-08 09:34:48 +0200

    Getting dom elems refactored
    Now all except chat bases on video elem

--- content.js
+++ content.js
@@ -4,19 +4,20 @@ function gotMessage(message,sender,sendresponse)
     alert("Extension started");
 
     var video = document.getElementsByTagName('video');
-    var parent = video[0].parentNode.parentNode;
+    var videoParent = video[0].parentNode;
+    var videoGrandParent = video[0].parentNode.parentNode;
 
     //var video = document.getElementsByClassName("_1c_u _5-yb _302m");
-    parent.style["width"] = "auto";
-    parent.style["height"] = "-webkit-fill-available";
+    videoGrandParent.style["width"] = "auto";
+    videoGrandParent.style["height"] = "-webkit-fill-available";
 
 
     var chat = document.getElementsByClassName("_6445 _6j32");
     chat[0].style['display'] = "none";
 
-    var parentDiv = document.getElementsByClassName("_53j5");
-    parentDiv[0].style['width'] = '-webkit-fill-available';
-    parentDiv[0].style['height'] = '95vh';
+    //var parentDiv = document.getElementsByClassName("_53j5");
+    videoParent.style['width'] = '-webkit-fill-available';
+    videoParent.style['height'] = '95vh';
 
     //parentDiv[0].style['height'] = '-webkit-fill-available';
 }
