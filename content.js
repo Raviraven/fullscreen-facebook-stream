fce93d85964bb276570b993accea9a6e76b913e9
Michal Kruszewski <kruszewski2@gmail.com>
2019-10-08 09:40:06 +0200

    Refactored lil' code

--- content.js
+++ content.js
@@ -6,11 +6,11 @@ function gotMessage(message,sender,sendresponse)
     var videoGrandParent = video[0].parentNode.parentNode;
     var chat = document.getElementsByClassName("_6445 _6j32");
  
-    videoGrandParent.style["width"] = "auto";
-    videoGrandParent.style["height"] = "-webkit-fill-available";
-
     chat[0].style['display'] = "none";
 
     videoParent.style['width'] = '-webkit-fill-available';
     videoParent.style['height'] = '95vh';
+
+    videoGrandParent.style["width"] = "auto";
+    videoGrandParent.style["height"] = "-webkit-fill-available";
 }
